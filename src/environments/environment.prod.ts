79cb0d568c18a54d521dda58cb8ca1eef97a5060
WaseemIsaacIbrahim <waseem.isaac@businessboomers.net>
2020-07-23 21:40:51 +0200

    fix image load failure

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   api_url: 'https://blogify-backend.herokuapp.com/api/v1',
-  basUrl : 'https://blogify-backend.herokuapp.com/'
+  baseUrl : 'https://blogify-backend.herokuapp.com/'
 };
