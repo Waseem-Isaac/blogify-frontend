6e874afe8a0b574bf4fcf74fadea754afed7dcae
AF-Waseem-Isaac <waseem.isaac@afaqy.com>
2021-04-14 14:29:59 +0200

    update environment.prod file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,8 @@
 export const environment = {
   production: true,
   api_url: 'https://blogify-backend.herokuapp.com/api/v1',
+  pusher: {
+    key: '365dcbf38c65b3376a80',
+    cluster: 'eu',
+  }
 };
